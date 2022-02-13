733eb8411d9fdc2748fc079ad9b4b0f708f1621d
aartsyfaartsy <kartik.a.gala@gmail.com>
2022-02-13 11:44:59 +0530

    commit

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
-<?php
 
-function lerningWordPress_resources(){
-	wp_enqueue_style( '<style', get_stylesheet_uri());
+ <?php  ?>
+function archifolio_scripts_loader() {
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	// 1. Styles.
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), $theme_version, 'all' );
 }
 
-add_action( 'wp_enqueue_script', 'LearningWordPress_resources');
+
+
