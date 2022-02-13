bd24edb9513816a594905706cacd28767ed40d7a
aartsyfaartsy <kartik.a.gala@gmail.com>
2022-02-13 12:32:09 +0530

    commit

--- functions.php
+++ functions.php
@@ -1,11 +1,7 @@
-
- <?php  ?>
-function archifolio_scripts_loader() {
-	$theme_version = wp_get_theme()->get( 'Version' );
-
-	// 1. Styles.
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), $theme_version, 'all' );
-}
+<?php  
 
 
 
+
+
+?>
